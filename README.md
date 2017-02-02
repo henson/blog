50893f2a3994bb7025e86db04c624d548aaeb223
henson <henson.lu@gmail.com>
2017-02-02 17:37:25 +0800

    2017-02-02

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ## Yupae
 
-[http://yupae.cn/markdown](http://yupae.cn/markdown)
-
-[Reference](http://www.yupae.cn/reference)
-
-[Test](http://www.yupae.cn/blog/markdown)
+* [http://yupae.cn/markdown](http://yupae.cn/markdown)
+* [Reference](http://www.yupae.cn/reference)
+* [Test](http://www.yupae.cn/blog/markdown)
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"os/exec"
+	"time"
+)
+
+var tempDate = time.Now().Format("2006-01-02")
+
+func main() {
+
+	//create markdown file
+	//writeMarkDown(tempDate, content)
+	println(tempDate + ".md is completed.")
+
+	//gitPull()
+	gitAddAll()
+	gitCommit()
+	gitPush()
+}
+
+func writeMarkDown(fileName, content string) {
+	// open output file
+	fo, err := os.Create(fileName + ".md")
+	if err != nil {
+		panic(err)
+	}
+	// close fo on exit and check for its returned error
+	defer func() {
+		if err := fo.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	// make a write buffer
+	w := bufio.NewWriter(fo)
+	w.WriteString(content)
+	w.Flush()
+}
+
+func gitPull() {
+	app := "git"
+	arg0 := "pull"
+	arg1 := "origin"
+	arg2 := "master"
+	cmd := exec.Command(app, arg0, arg1, arg2)
+	out, err := cmd.Output()
+
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	print(string(out))
+}
+
+func gitAddAll() {
+	app := "git"
+	arg0 := "add"
+	arg1 := "."
+	cmd := exec.Command(app, arg0, arg1)
+	out, err := cmd.Output()
+
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	print(string(out))
+}
+
+func gitCommit() {
+	app := "git"
+	arg0 := "commit"
+	arg1 := "-am"
+	arg2 := tempDate
+	cmd := exec.Command(app, arg0, arg1, arg2)
+	out, err := cmd.Output()
+
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	print(string(out))
+}
+
+func gitPush() {
+	app := "git"
+	arg0 := "push"
+	arg1 := "origin"
+	arg2 := "master"
+	cmd := exec.Command(app, arg0, arg1, arg2)
+	out, err := cmd.Output()
+
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	print(string(out))
+}
